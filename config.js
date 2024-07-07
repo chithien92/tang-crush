b360d97fb8cf4d818301342e78a23e0e0e02ef0e
chithien92 <xuan44019@gmail.com>
2024-07-08 00:52:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink : https://nguyenchithien.github.io/tang-crush-/
+    messLink : https://chithien92.github.io/tang-crush/
 }
